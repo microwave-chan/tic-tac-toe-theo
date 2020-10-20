67ac1bc90b7e423eb068a5381f5e12a0c6dfb2c9
microwave-chan <72061352+microwave-chan@users.noreply.github.com>
2020-10-20 13:38:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 
 rowOne = ["1", "|", "2", "|", "3"]
+rowTwo = ["1", "|", "2", "|", "3"]
+rowThree= ["1", "|", "2", "|", "3"]
+
 
 Seperator = "-+-+-"
 
-for i in range len(rowOne):
-  print(rowOne[i])
+gameRunning = True
+
+currentturn = "X"
+
+while gameRunning:
+  print(rowOne)
+  print(Seperator)
+  print(rowTwo)
+  print(Seperator)
+  print(rowThree)
+  
+  input(currentturn + ", it is your turn)
+        
+  if input == "1":
+        rowOne[0] = currentturn
+  elif input == #whatever"
+        #replace
+        
+  #continue until you have all the replacing done
+        
+  if currentturn = "X":
+        currentturn = "O"
+  else:
+        currentturn"X"
+  
+  
